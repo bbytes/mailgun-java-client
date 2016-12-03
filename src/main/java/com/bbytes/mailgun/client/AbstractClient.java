13a6995bf2f9b716daaa3b000298e0a023bfbd9d
tanmally <tm@beyondbytes.co.in>
2016-12-03 16:33:48 +0530

    Fixed mail gun 400 request issue - base 64 byte conversion issue

--- src/main/java/com/bbytes/mailgun/client/AbstractClient.java
+++ src/main/java/com/bbytes/mailgun/client/AbstractClient.java
@@ -1,10 +1,15 @@
 package com.bbytes.mailgun.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.AsyncClientHttpRequestExecution;
@@ -30,10 +35,12 @@ import org.springframework.web.client.RestTemplate;
 
 public abstract class AbstractClient {
 
+	private final static Logger log = LoggerFactory.getLogger(AbstractClient.class);
+
 	protected static Integer TIMEOUT_IN_SECS = 30;
 
 	protected RestTemplate restTemplate;
-	
+
 	protected AsyncRestTemplate asyncRestTemplate;
 
 	protected String apiKey;
@@ -56,13 +63,15 @@ public abstract class AbstractClient {
 		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
 		factory.setReadTimeout(getReadTimeoutInSecs() * 1000);
 		factory.setConnectTimeout(getConnectionTimeoutInSecs() * 1000);
+		factory.setConnectionRequestTimeout(getConnectionTimeoutInSecs() * 1000);
 		return factory;
 	}
-	
+
 	private AsyncClientHttpRequestFactory asyncClientHttpRequestFactory() {
 		HttpComponentsAsyncClientHttpRequestFactory factory = new HttpComponentsAsyncClientHttpRequestFactory();
 		factory.setReadTimeout(getReadTimeoutInSecs() * 1000);
 		factory.setConnectTimeout(getConnectionTimeoutInSecs() * 1000);
+		factory.setConnectionRequestTimeout(getConnectionTimeoutInSecs() * 1000);
 		return factory;
 	}
 
@@ -82,7 +91,7 @@ public abstract class AbstractClient {
 		List<ClientHttpRequestInterceptor> interceptors = getRestTemplate().getInterceptors();
 		interceptors.add(new HTTPBasicAuthRequestInterceptor(apiKey));
 		getRestTemplate().setInterceptors(interceptors);
-		
+
 		List<AsyncClientHttpRequestInterceptor> asyncInterceptors = getAsyncRestTemplate().getInterceptors();
 		asyncInterceptors.add(new HTTPBasicAuthRequestInterceptor(apiKey));
 		getAsyncRestTemplate().setInterceptors(asyncInterceptors);
@@ -128,8 +137,7 @@ public abstract class AbstractClient {
 		converter.setCharset(Charset.forName("UTF-8"));
 		List<HttpMessageConverter<?>> partConverters = new ArrayList<HttpMessageConverter<?>>();
 		partConverters.add(new ByteArrayHttpMessageConverter());
-		StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(
-				Charset.forName("UTF-8"));
+		StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
 		stringHttpMessageConverter.setWriteAcceptCharset(false);
 		partConverters.add(stringHttpMessageConverter);
 		partConverters.add(new ResourceHttpMessageConverter());
@@ -174,12 +182,12 @@ public abstract class AbstractClient {
 	protected RestTemplate getRestTemplate() {
 		return restTemplate;
 	}
-	
+
 	protected AsyncRestTemplate getAsyncRestTemplate() {
 		return asyncRestTemplate;
 	}
 
-	private static final class HTTPBasicAuthRequestInterceptor implements ClientHttpRequestInterceptor , AsyncClientHttpRequestInterceptor{
+	private static final class HTTPBasicAuthRequestInterceptor implements ClientHttpRequestInterceptor, AsyncClientHttpRequestInterceptor {
 
 		private final String password;
 		private final String username = "api";
@@ -188,28 +196,63 @@ public abstract class AbstractClient {
 			this.password = apiKey;
 		}
 
-		public ClientHttpResponse intercept(final HttpRequest request, final byte[] body,
-				ClientHttpRequestExecution execution) throws IOException {
+		public ClientHttpResponse intercept(final HttpRequest request, final byte[] body, ClientHttpRequestExecution execution)
+				throws IOException {
 
-			String plainCreds = username + ":" + password;
+			String plainCreds = username + ":" + password.trim();
 			byte[] plainCredsBytes = plainCreds.getBytes();
-			byte[] base64CredsBytes = Base64Utils.encode(plainCredsBytes);
-			String base64Creds = new String(base64CredsBytes);
-
+			String base64Creds = Base64Utils.encodeToString(plainCredsBytes).trim();
+			
 			request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Basic " + base64Creds);
-			return execution.execute(request, body);
+			traceRequest(request, body);
+			ClientHttpResponse response = execution.execute(request, body);
+			traceResponse(response);
+			return response;
 		}
 
 		@Override
 		public ListenableFuture<ClientHttpResponse> intercept(HttpRequest request, byte[] body, AsyncClientHttpRequestExecution execution)
 				throws IOException {
-			String plainCreds = username + ":" + password;
+			String plainCreds = username + ":" + password.trim();
 			byte[] plainCredsBytes = plainCreds.getBytes();
-			byte[] base64CredsBytes = Base64Utils.encode(plainCredsBytes);
-			String base64Creds = new String(base64CredsBytes);
+			String base64Creds = Base64Utils.encodeToString(plainCredsBytes).trim();
 
 			request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Basic " + base64Creds);
-			return execution.executeAsync(request, body);
+			traceRequest(request, body);
+			ListenableFuture<ClientHttpResponse> response = execution.executeAsync(request, body);
+			try {
+				traceResponse(response.get());
+			} catch (InterruptedException | ExecutionException e) {
+				log.error(e.getMessage(), e);
+			}
+			return response;
+
+		}
+
+		private void traceRequest(HttpRequest request, byte[] body) throws IOException {
+			log.debug("===========================request begin================================================");
+			log.debug("URI         : {}", request.getURI());
+			log.debug("Method      : {}", request.getMethod());
+			log.debug("Headers     : {}", request.getHeaders());
+			log.debug("Request body: {}", new String(body, "UTF-8"));
+			log.debug("==========================request end================================================");
+		}
+
+		private void traceResponse(ClientHttpResponse response) throws IOException {
+			StringBuilder inputStringBuilder = new StringBuilder();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody(), "UTF-8"));
+			String line = bufferedReader.readLine();
+			while (line != null) {
+				inputStringBuilder.append(line);
+				inputStringBuilder.append('\n');
+				line = bufferedReader.readLine();
+			}
+			log.debug("============================response begin==========================================");
+			log.debug("Status code  : {}", response.getStatusCode());
+			log.debug("Status text  : {}", response.getStatusText());
+			log.debug("Headers      : {}", response.getHeaders());
+			log.debug("Response body: {}", inputStringBuilder.toString());
+			log.debug("=======================response end=================================================");
 		}
 
 	}
